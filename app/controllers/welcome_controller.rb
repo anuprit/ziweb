2b6aa916c78a837aac30b4574eada33ae321cb4c
Anuprit <anuprit@gmail.com>
2013-03-04 16:46:10 -0800

    showing only two projects

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 class WelcomeController < ApplicationController
 	def index
-		@posts = Post.order('created_at desc').all
+		@posts = Post.order('created_at desc').limit(2)
 	end
 
 	def publications
