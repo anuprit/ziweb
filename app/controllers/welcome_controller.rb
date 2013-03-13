79ad72780c2ff9f860d8d16113c8784f92bb5590
Anuprit <anuprit@gmail.com>
2013-03-13 00:38:13 -0700

    added misc

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,7 @@
 class WelcomeController < ApplicationController
 	def index
 		@posts = Post.order('created_at desc').limit(2)
+		@minis = Mini.order('created_at desc').limit(2)
 	end
 
 	def publications
